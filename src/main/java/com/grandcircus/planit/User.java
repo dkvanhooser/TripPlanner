fc014bf8b057087c346e563f185af2ef7cbc829f
dkvanhooser <dkvanhooser@gmail.com>
2016-09-16 14:16:49 -0400

    configuring hibernate settings

---
+++ src/main/java/com/grandcircus/planit/User.java
@@ -0,0 +1,56 @@
+package com.grandcircus.planit;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class User {
+	@NotEmpty
+	private String username;
+	@NotEmpty
+	private String password;
+	@Email
+	@NotEmpty
+	private String email;
+	private String fullname;
+	private int ID;
+	private int permission;
+	
+	public int getID() {
+		return ID;
+	}
+	public void setID(int ID) {
+		this.ID = ID;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public int getPermission() {
+		return permission;
+	}
+	public void setPermission(int permission) {
+		this.permission = permission;
+	}
+
+}
